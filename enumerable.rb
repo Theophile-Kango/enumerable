218a2cff972b55aa20a62fe6c25eee986d9ab383
Theophile-Kango <fadhili.kango@gmail.com>
2020-02-28 16:01:58 +0200

    Implemente the each_with_index funcion

--- enumerable.rb
+++ enumerable.rb
@@ -11,6 +11,19 @@ module Enumerable
       self
     end
   end
+
+  def my_each_with_index
+    unless block_given?
+      to_enum(__method__)
+    else
+      i = 0
+      while i < self.size
+        yield(self[i], i)
+        i += 1
+      end
+      self
+    end
+  end
 end
 
 
@@ -18,4 +31,10 @@ array = ["Theo","Gloria","Veronica"]
 
 print array.my_each
 
-
+hash = Hash.new
+%w(cat dog wombat).my_each_with_index { |item, index|
+  hash[item] = index
+}
+p hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
+p array.my_each_with_index
+p array.each_with_index
