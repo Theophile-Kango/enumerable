97ed47a6b2ec1d0ad61dbf0ebb01a04bfec72b2f
Theophile-Kango <fadhili.kango@gmail.com>
2020-03-04 13:21:13 +0200

    correct the readme and enumerable files according to TSE's recommendations

--- enumerable.rb
+++ enumerable.rb
@@ -35,7 +35,7 @@ module Enumerable
       while i < self.size
         yield self[i]
         if yield self[i]
-          arr.push(self[i])
+          arr << self[i]
         end
         i += 1
       end
@@ -47,6 +47,7 @@ module Enumerable
     unless block_given?
       i = 0
       j = 0
+      k = 0
       while i < self.size
         self[i]
         i += 1
@@ -70,17 +71,46 @@ module Enumerable
           end
         end
         j == self.length ? true : false
+      elsif word != nil
+        i = 0
+        while i < self.size
+          if self[i - 1] == self[i]
+            k += 1
+          end
+          i += 1
+        end
+
+        for element in self
+          if element.class == word
+            j += 1
+          end
+        end
+        j == self.length ? true : false
+        k == self.length ? true : false
+
       else
-        false
+        for element in self do
+          if element == false or element == nil
+            k += 1
+          end
+        end
+        k == 0 ? true : false
       end
     else
       i = 0
+      j = 0
       while i < self.size
         yield self[i]
         i += 1
       end
 
-      yield(self) ? true : false
+      for element in self
+
+        if yield(element) == false or yield(element) == nil
+          j += 1
+        end
+      end
+      j == 0 ? true : false
 
     end
   end
@@ -105,7 +135,23 @@ module Enumerable
         j > 0 and j <= self.length ? true : false
 
       elsif word != nil
+        i = 0
+        k = 0
+        while i < self.size
+          if self[i - 1] == self[i]
+            k += 1
+          end
+          i += 1
+        end
 
+        for element in self
+          if element == word
+            k += 1
+          end
+        end
+        k > 0 ? true : false
+
+      elsif word == Numeric
         for element in self
           if element.class <= word
             j += 1
@@ -115,8 +161,16 @@ module Enumerable
         j > 0 and j <= self.length ? true : false
 
       else
-        true
+        k = 0
+        for element in self do
+          if element == false or element == nil
+            k += 1
+          end
+        end
+        k != self.size ? true : false
+
       end
+
     else
       i = 0
       j = 0
@@ -154,14 +208,25 @@ module Enumerable
         j == 0 ? true : false
 
       elsif word != nil
-
-        for element in self
-          if element.class <= word
-            j += 1
+        if word == Numeric
+          for element in self
+            if element.class <= word
+              j += 1
+            end
           end
-        end
 
-        j != 0 ? false : true
+          j != 0 ? false : true
+        else
+          i = 0
+          k = 0
+          while i < self.size
+            if self[i] == word
+              k += 1
+            end
+            i += 1
+          end
+          k == 0 ? true : false
+      end
 
       else
         j = 0
@@ -194,12 +259,13 @@ module Enumerable
 
       if val.class != NilClass
         i = 0
-        self.my_each do |count|
-          if self[count] == val
+
+        for el in self do
+          if el == val
             i += 1
           end
         end
-        val
+        i
       else
         self.my_each do |count|
           count += 1
@@ -224,7 +290,7 @@ module Enumerable
       arr = []
       for element in self do
         current = yield element
-        arr.push(current)
+        arr << current
       end
       arr
     end
@@ -232,45 +298,44 @@ module Enumerable
 
   def my_inject(acc = nil)
     arr = []
-    arr2 = []
-
-    for element in self do
-      if element.class == String
-        result = yield(element, element)
-      else
-        result = yield(element, element.next)
-      end
-      arr.push(result)
-    end
-
     i = 0
-    j = 0
-    while i < arr.length do
-      if arr[i].class == String
-        arr2.push(arr[i])
-      else
-        if i.even?
-          arr2.push(arr[i])
+    for element in self do
+      arr << element
+    end
+
+    unless block_given?
+
+      if acc.class == Symbol
+        case acc
+        when :+
+          result = 0
+        when :-
+          result = 0
+        else
+          result = 1
         end
-    end
-      i += 1
-    end
-
-    result = yield(arr2[0], arr2[1])
-
-    arr2.my_each do |e|
-      if e.class == String
-        result = yield(result, e)
+        for el in arr do
+          result = result.send(acc, el)
+        end
       end
+    else
 
-      if j > 1
-        result = yield(result, e)
+      result = yield(arr[0], arr[1])
+
+      arr.my_each do |e|
+        if e.class == String
+          result = yield(result, e)
+        end
+
+        if i > 1
+          result = yield(result, e)
+        end
+        i += 1
       end
-      j += 1
-    end
-    if acc.class <= Numeric
-      result = result * acc
-    end
+      if acc.class <= Numeric
+        result = result * acc
+      end
+  end
     result
   end
 
@@ -282,81 +347,3 @@ module Enumerable
     my_map
   end
 end
-
-# 1) my_select
-
-# p [1,2,3,4,5].my_select { |num|  num.even?  }   #=> [2, 4]
-
-# array = ["Theo","Gloria","Veronica"]
-
-# 2) my_each
-
-# p array.my_each
-
-# 3) my_each_with_index
-
-# hash = Hash.new
-# %w(cat dog wombat).my_each_with_index { |item, index|
-#   hash[item] = index
-# }
-# p hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
-# p array.my_each_with_index
-# p array.each_with_index
-
-# 4) my_all?
-
-# p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
-# p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
-# p [].my_all? #=> true
-# p %w[ant bear cat].my_all?(/t/)                        #=> false
-# p [1, 2i, 3.14].my_all?(Numeric)                       #=> true
-# p [nil, true, 99].my_all?                              #=> false
-
-# #5) my_any?
-
-# p %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
-# p %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
-# p %w[ant bear cat].my_any?(/d/)                        #=> false
-# p [nil, true, 99].my_any?(Integer)                     #=> true
-# p [nil, true, 99].my_any?                              #=> true
-# p [].my_any?                                           #=> false
-
-# 6) my_none?
-
-# p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
-# p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
-# p %w{ant bear cat}.my_none?(/d/)                        #=> true
-# p [1, 3.14, 42].my_none?(Float)                         #=> false
-# p [].my_none?                                           #=> true
-# p [nil].my_none?                                        #=> true
-# p [nil, false].my_none?                                 #=> true
-# p [nil, false, true].my_none?                           #=> false
-
-# 7) my_count
-# ary = [1, 2, 4, 2]
-# p ary.my_count               #=> 4
-# p ary.my_count(2)            #=> 2
-# p ary.my_count{ |x| x%2==0 } #=> 3
-
-# 8) my_map
-# p (1..4).my_map
-# p (1..4).my_map { |i| i*i }      #=> [1, 4, 9, 16]
-# p (1..4).my_map { "cat"  }   #=> ["cat", "cat", "cat", "cat"]
-
-# 9) my_inject
-
-# p (5..10).my_inject { |sum, n| sum + n } #=> 45
-# p (5..10).my_inject(1) { |product, n| product * n } #=> 151200
-
-# longest = %w{cat sheep bear}.my_inject do |memo, word|
-#   memo.length > word.length ? memo : word
-# end
-# p longest
-
-# 10) multiply_els
-
-# p (5..10).multiply_els
-
-# 11) my_map_proc
-
- #p (1..4).my_map_proc
